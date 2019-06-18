28178616e17cbaa79d3ea2744ad4838f59cccc12
bitflower <matthias.max@bitflower.net>
2019-06-18 17:03:42 +0200

    use src files

--- src/graph/auth-provider.ts
+++ src/graph/auth-provider.ts
@@ -1,5 +1,6 @@
 import { AuthenticationProvider } from '@microsoft/microsoft-graph-client';
-import * as MSALAuthenticationProvider from '@microsoft/microsoft-graph-client/lib/es/browser/MSALAuthenticationProvider';
+// import * as MSALAuthenticationProvider from '@microsoft/microsoft-graph-client/lib/es/browser/MSALAuthenticationProvider';
+import * as MSALAuthenticationProvider from '@microsoft/microsoft-graph-client/lib/src/MSALAuthenticationProvider';
 
 export let authProvider: AuthenticationProvider;
 
