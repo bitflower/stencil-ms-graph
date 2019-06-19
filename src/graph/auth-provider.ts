283259dd016e2b96d34a0ca29905e13e16d2d666
bitflower <matthias.max@bitflower.net>
2019-06-19 14:23:33 +0200

    update to graph-client v2.0

--- src/graph/auth-provider.ts
+++ src/graph/auth-provider.ts
@@ -1,13 +1,14 @@
 import { AuthenticationProvider } from '@microsoft/microsoft-graph-client';
-// import * as MSALAuthenticationProvider from '@microsoft/microsoft-graph-client/lib/es/browser/MSALAuthenticationProvider';
-import * as MSALAuthenticationProvider from '@microsoft/microsoft-graph-client/lib/src/MSALAuthenticationProvider';
+import * as MSALAuthenticationProvider from '@microsoft/microsoft-graph-client/lib/es/browser/ImplicitMSALAuthenticationProvider';
 
 export let authProvider: AuthenticationProvider;
 
 export const getAuthProvider = (userAgentApplication, scopes) => {
   console.log('getAuthProvider');
-  authProvider = new MSALAuthenticationProvider.MSALAuthenticationProvider(
+  authProvider = new MSALAuthenticationProvider.ImplicitMSALAuthenticationProvider(
     userAgentApplication,
-    scopes
+    {
+      scopes
+    }
   );
 };
